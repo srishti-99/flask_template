587ef45771d9904867b7b8e8291bca131b3cbcdf
srishti-99 <59068717+srishti-99@users.noreply.github.com>
2020-08-20 03:43:49 +0530

    added static folder, updated templates accordingly

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,26 +36,34 @@ def create_app(test_config=None):
         dataObj.update_df()
         return render_template('user_table.html', tables=[dataObj.df.reset_index()])
 
+    
     @app.route('/insert', methods=['GET', 'POST'])
     def insert_user(): 
+        """
+        Send username, firstname, lastname, description to html form as a post request
+        """
         dataObj.update_df()
         username = request.form.get("user_name","").lower().replace(" ", "_")
         ids = list(dataObj.df.index.values)
-
-        if username != "" and username not in ids:
-            flash("Username: " + username)
-            description = request.form.get("description","")
-            firstname = request.form.get("firstname","")
-            lastname = request.form.get("lastname","")
-            db.insert_user(firstname, lastname, username, description)
-            return redirect('/users')
-        elif username in ids:
-            flash("This username already exists. Please try again with different username")
+        
+        if request.method == 'POST':
+            if username != "" and username not in ids:
+                flash("Username: " + username)
+                description = request.form.get("description","")
+                firstname = request.form.get("firstname","")
+                lastname = request.form.get("lastname","")
+                db.insert_user(firstname, lastname, username, description)
+                return redirect('/users')
+            elif username in ids:
+                flash("This username already exists. Please try again with different username")
 
         return render_template('insert.html', users=ids)
 
     @app.route('/update/<username>', methods=['GET', 'POST'])
     def update(username):
+        """
+        Send username, firstname, lastname, description to html form as a post request
+        """
         dataObj.update_df()
         username = username.lower().replace(" ", "_")
         ids = list(dataObj.df.index.values)
@@ -75,13 +83,16 @@ def create_app(test_config=None):
             db.update_user(firstname, lastname, username, description)
             flash("Your data was updated")
             return redirect('/users')       
-        elif request.method == 'POST' and username not in ids: 
+        elif request.method == 'PUT' and username not in ids: 
             flash("This username does not exist. Choose one from this list to update:")
             return render_template('user_table.html', tables=[dataObj.df.reset_index()])
         return render_template('update.html', username=username)
 
     @app.route('/delete/<username>', methods=['GET', 'POST'])
     def delete(username): 
+        """
+        Send deletion to html form as a post request 
+        """
         dataObj.update_df()
         username = username.lower().replace(" ", "_")
         ids = list(dataObj.df.index.values)
